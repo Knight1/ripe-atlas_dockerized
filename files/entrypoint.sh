8b3632e79e4740a61b4e6c57cf006f8859bdf9d5
Tobias "Knight" S <tobias@tobiassachs.de>
2019-11-05 06:18:53 +0100

    Fix for GitHub not supporting IPv6

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -4,4 +4,6 @@ set -Eeo pipefail
 chown -R atlas:atlas /var/atlas-probe/status
 chown -R atlas:atlas /var/atlas-probe/etc
 
+if [ -z ${GITHUB_ACTION+x} ]; then :; else echo "REG_1_HOST=193.0.19.246" > /var/atlas-probe/bin/reg_servers.sh; fi
+
 exec "$@"
